0cf499993b54bc7347e39fede54b8cfdc193ed49
TongNPZ <tong.naja101@gmail.com>
2024-04-02 16:24:24 +0700

    eslint

--- src/app/componnent/Navbar/navBar.js
+++ src/app/componnent/Navbar/navBar.js
@@ -3,21 +3,21 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-<nav className="navbar navbar-expand-lg navbar-dark navbar-color">
+    <nav className="navbar navbar-expand-lg navbar-dark navbar-color">
       <div className="container">
         <Link href="/">
-          <p className="navbar-brand">Navbar</p>
+          <p className="navbar-brand">หน้าหลัก</p>
         </Link>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link href="/">
-                <p className="nav-link">Home</p>
+                <p className="nav-link">โครงการ</p>
               </Link>
             </li>
             <li className="nav-item">
               <Link href="/about">
-                <p className="nav-link">About</p>
+                <p className="nav-link">ติดต่อเรา</p>
               </Link>
             </li>
             {/* เพิ่มเมนูเพิ่มเติมตามต้องการ */}
