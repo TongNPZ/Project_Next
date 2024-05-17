4c74f6fd20472a2ed32dbbe890051818b951e619
TongNPZ <tong.naja101@gmail.com>
2024-05-17 17:52:40 +0700

    fixbug contract Complete!!!!

--- src/app/componnent/pdfComponent/Contract.js
+++ src/app/componnent/pdfComponent/Contract.js
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
     },
     textContent: {
         fontSize: 10,
-        marginBottom: 10,
+        marginBottom: 5,
         textAlign: 'justify',
         lineHeight: '1.8', // กำหนดระยะห่างระหว่างบรรทัดที่นี่
 
@@ -62,6 +62,11 @@ const styles = StyleSheet.create({
     textContentBold: {
         fontFamily: 'SarabunBold',
         fontSize: 10,
+    },
+    textSubContent: {
+        fontSize: 10,
+        lineHeight: '1.8', // กำหนดระยะห่างระหว่างบรรทัดที่นี่
+        // textAlign: 'justify',
     },
 
 });
@@ -92,22 +97,25 @@ const DocContract = ({ housingEstate, contract }) => (
             {housingEstate.map((data) => (
                 <View key={data.he_id}>
                     <Text style={[styles.textContent, styles.textWithIndent]}>
-                        ก. {data.company} โดย {data.md_name} กรรมการผู้จัดการ สัญชาติ {data.md_nationality} บ้านเลขที่ {data.md_address} โทร. {data.phone} ซึ่งต่อไปในสัญญานี้จะเรียกว่า "ผู้จะขาย" ฝ่ายหนึ่งกับ
+                        ก.&nbsp;{data.company} โดย {data.md_name} กรรมการผู้จัดการ สัญชาติ {data.md_nationality} บ้านเลขที่ {data.md_address} โทร. {data.phone} ซึ่งต่อไปในสัญญานี้จะเรียกว่า "ผู้จะขาย" ฝ่ายหนึ่งกับ
                     </Text>
                     <Text style={[styles.textContent, styles.textWithIndent]}>
-                        ข. {contract.user_name} {contract.user_lastname} อายุ {contract.user_age} ปี สัญชาติ {contract.nationality} {contract.user_address} โทรศัพท์ {contract.user_phone} ถือบัตรประชาชนเลขที่ {contract.user_id} ออกโดย กระทรวงมหาดไทย ซึ่งต่อไปในสัญญานี้จะเรียกว่า "ผู้จะซื้อ" ฝ่ายหนึ่ง
+                        ข.&nbsp;{contract.user_name} {contract.user_lastname} อายุ {contract.user_age} ปี สัญชาติ {contract.nationality} {contract.user_address} โทรศัพท์ {contract.user_phone} ถือบัตรประชาชนเลขที่ {contract.user_id} ออกโดย กระทรวงมหาดไทย ซึ่งต่อไปในสัญญานี้จะเรียกว่า "ผู้จะซื้อ" ฝ่ายหนึ่ง
                     </Text>
                     <Text style={[styles.textContent, styles.textWithIndent]}>
                         ทั้งสองฝ่ายได้ตกลงทำสัญญากันมีข้อความดังต่อไปนี้
                     </Text>
                     <Text style={[styles.textContent, styles.textWithIndent]}>
-                        ข้อ1. ผู้จะขายตกลงจะขายและผู้จะซื้อตกลงจะซื้อที่ดิน โฉนดที่ดินเลขที่ {contract.num_deed} หน้าสำรวจ {contract.num_survey} หมู่บ้าน{data.name} บ้านเลขที่ {data.address} (แปลงเลขที่ {contract.house_no}) ขนาดเนื้อที่ประมาณ {contract.hLand_space} ตารางวา ในราคา {parseFloat(contract.price).toLocaleString()} บาท ({THBText(contract.price)}) หากภายหลังปรากฏว่าที่ดินเพิ่มขึ้นหรือลดลงจากที่ระบุไว้ตามสัญญานี้ คู่สัญญาตกลงเพิ่มหรือลดเงินในราคาตารางวาละ {parseFloat(contract.land_price).toLocaleString()} บาท ({THBText(contract.land_price)})
+                        ข้อ1.&nbsp;ผู้จะขายตกลงจะขายและผู้จะซื้อตกลงจะซื้อที่ดิน โฉนดที่ดินเลขที่ {contract.num_deed} หน้าสำรวจ {contract.num_survey} หมู่บ้าน{data.name} บ้านเลขที่ {data.address} (แปลงเลขที่ {contract.house_no}) ขนาดเนื้อที่ประมาณ {contract.hLand_space} ตารางวา ในราคา {parseFloat(contract.price).toLocaleString()} บาท ({THBText(contract.price)}) หากภายหลังปรากฏว่าที่ดินเพิ่มขึ้นหรือลดลงจากที่ระบุไว้ตามสัญญานี้ คู่สัญญาตกลงเพิ่มหรือลดเงินในราคาตารางวาละ {parseFloat(contract.land_price).toLocaleString()} บาท ({THBText(contract.land_price)})
                     </Text>
                     <Text style={[styles.textContent, styles.textWithIndent]}>
-                        ข้อ2. ผู้จะซื้อตกลงชำระเงินมัดจำค่าที่ดินให้แก่ผู้จะขายและผู้จะขายได้รับเงินมัดจำดังกล่าวไว้แล้วเป็นเงิน {parseFloat(contract.b_amount).toLocaleString()} บาท ({THBText(contract.b_amount)}) และในวันทำสัญญานี้ผู้จะซื้อได้ชำระเงินจำนวน {parseFloat(contract.con_amount).toLocaleString()} บาท ส่วนที่เหลืออีกจำนวน {parseFloat((contract.price - contract.b_amount) - contract.con_amount).toLocaleString()} บาท ({THBText((contract.price - contract.b_amount) - contract.con_amount)}) ชำระภายในวันโอนหรือภายในเจ็ดวันหลังจากได้รับแจ้งให้ไปชำระส่วนที่เหลือและทำนิติกรรมรับโอนกรรมสิทธิ์ที่ดิน จากผู้จะขายการชำระเงินหากชำระด้วยเช็ค ตั๋วเงิน หรือตราสารใดๆ  นอกจากชำระด้วยเงินสดแล้วจะถือว่าผู้จะซื้อได้ชำระเสร็จเรียบร้อยแล้วในงวดนั้นๆ ต่อเมื่อเช็ค ตั๋วเงิน หรือตราสารใดๆ นั้นขึ้นเงิน และผ่านเข้าบัญชีของผู้จะขายเรียบร้อยแล้ว &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        ข้อ2.&nbsp;ผู้จะซื้อตกลงชำระเงินมัดจำค่าที่ดินให้แก่ผู้จะขายและผู้จะขายได้รับเงินมัดจำดังกล่าวไว้แล้วเป็นเงิน&nbsp;{parseFloat(contract.b_amount).toLocaleString()} บาท&nbsp;({THBText(contract.b_amount)})&nbsp;และในวันทำสัญญานี้ผู้จะซื้อได้ชำระเงินจำนวน&nbsp;{parseFloat(contract.con_amount).toLocaleString()}&nbsp;บาท&nbsp;ส่วนที่เหลืออีกจำนวน&nbsp;{parseFloat((contract.price - contract.b_amount) - contract.con_amount).toLocaleString()}&nbsp;บาท&nbsp; ({THBText((contract.price - contract.b_amount) - contract.con_amount)})&nbsp;ชำระภายในวันโอนหรือภายในเจ็ดวันหลังจากได้รับแจ้งให้ไปชำระส่วนที่เหลือและ &nbsp;
+                        ทำนิติกรรมรับโอนกรรมสิทธิ์ที่ดิน&nbsp;ตั๋วเงิน&nbsp;หรือตราสารใดๆ&nbsp;นอกจากชำระด้วยเงินสดแล้วจะถือว่าผู้จะซื้อได้ชำระเสร็จเรียบร้อยแล้ว ในงวดนั้นๆ
+                        &nbsp;ต่อเมื่อเช็ค&nbsp;ตั๋วเงิน&nbsp;หรือตราสารใดๆ&nbsp;นั้นขึ้นเงิน&nbsp;และผ่านเข้าบัญชีของผู้จะขายเรียบร้อยแล้ว&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     </Text>
                 </View>
             ))}
+            {/* ชำระภายในวันโอนหรือภายในเจ็ดวันหลังจากได้รับแจ้งให้ไปชำระส่วนที่เหลือและทำนิติกรรมรับโอนกรรมสิทธิ์ที่ดิน จากผู้จะขายการชำระเงินหากชำระด้วยเช็ค ตั๋วเงิน หรือตราสารใดๆ  นอกจากชำระด้วยเงินสดแล้วจะถือว่าผู้จะซื้อได้ชำระเสร็จเรียบร้อยแล้วในงวดนั้นๆ ต่อเมื่อเช็ค ตั๋วเงิน หรือตราสารใดๆ นั้นขึ้นเงิน และผ่านเข้าบัญชีของผู้จะขายเรียบร้อยแล้ว &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
 
         </Page>
     </Document>
